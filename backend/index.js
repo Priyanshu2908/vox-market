7bca10ddfd51d0578e22e8ebf260f8b494ab3b50
lokeshcoo <lokeshmisra2813@gmail.com>
2025-03-02 13:35:46 +0530

    Add user router and update product model; configure dependencies and server settings

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-// const userRouter = require('./routers/UserRouter');
+const userRouter = require('./routers/UserRouter');
 const cors = require('cors');
 
 const app = express();
 
-const port = 6000;
+const port = 5000;
 
 // middleware
-app.use(cors({ origin: ['http://localhost:3000'] }));
+// app.use(cors({ origin: ['http://localhost:3000'] }));
 app.use(express.json());
 // app.use('/user', userRouter);
 
