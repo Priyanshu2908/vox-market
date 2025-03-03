7c1b8a84fab3e6068d1d03981ddcaf7d9a16ca1a
yash-nomadic <yashsingh4537@gmail.com>
2025-03-03 13:41:15 +0530

    Enable CORS for localhost in backend middleware

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 5000;
 
 // middleware
-// app.use(cors({ origin: ['http://localhost:3000'] }));
+app.use(cors({ origin: ['http://localhost:3000'] }));
 app.use(express.json());
 app.use('/user', userRouter);
 
