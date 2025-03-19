406cb7b691ce36676ed4aeb99a982d507006b483
yash-nomadic <yashsingh4537@gmail.com>
2025-03-19 13:19:42 +0530

    feat: add utility functions and configure Tailwind CSS with custom themes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
     return (
 
-        <div className="min-h-screen flex flex-col items-center justify-center bg-white  p-6">
+        <div className="min-h-screen flex flex-col items-center justify-center bg-white p-6">
             <div className="bg-white shadow-lg rounded-2xl p-10 text-center max-w-md w-full">
                 <h1 className="text-3xl font-bold mb-6">Welcome to Our QuicKart</h1>
                 <p className="text-gray-600 mb-8">Login as a User or a Seller to get started.</p>
